2791062b26f188758cd16d6fbbea28ce0e66010c
xMerkery <mw3.noah@gmail.com>
2024-06-10 21:33:01 -0400

    Initial Commit

---
+++ fitApp/fitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitAppApp.swift
+//  fitApp
+//
+//  Created by Noah on 2024-06-10.
+//
+
+import SwiftUI
+
+@main
+struct fitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
